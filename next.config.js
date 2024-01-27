60f477a97dd27438be25fee70f78a24e399fb2df
nikdelvin <erter.live@gmail.com>
2024-01-27 19:58:03 +0400

    Deploy fix

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'standalone',
     reactStrictMode: false,
     env: {
         GOOGLE_APPLICATION_CREDENTIALS: './credentials.json',
